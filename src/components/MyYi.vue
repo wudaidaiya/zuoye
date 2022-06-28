d155d42fd677ef24d8a75051d7af57111df08930
wudaidai <1665334372@qq.com>
2022-06-28 19:32:47 +0800

    新建文件夹

---
+++ src/components/MyYi.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>{{mag}}
+  <h1>{{obj.name}}</h1>
+  <h1>{{obj.age}}</h1>
+  <h1>{{obj.age>=18?'上网去':'回家'}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name:'App',
+data(){
+    return{
+        msg:'你好你好',
+        obj: {
+            name:'小弟',
+            age:16
+        }
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,// 关闭eslint检查
+  devServer: { // 自定义服务配置
+    open: true, // 自动打开浏览器
+    host: 'localhost', 
+  }
 })
