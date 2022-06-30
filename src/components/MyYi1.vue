474138a5306b6cc9b5543aaf8efd6d01a59d0888
wudaidai <1665334372@qq.com>
2022-06-30 19:24:19 +0800

    sansan

---
+++ src/components/MyYi1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+    <h1 v-show="isShow">我是h1</h1>
+    <h2 v-if="isOk">我是h2</h2>
+    
+     <p v-if="age >= 18">成年了</p>
+     <p v-else>未成年</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        isShow: false,
+        isOk: false,
+        age: 2
+    }
+  }
+}
+</script>
