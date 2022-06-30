b4690c6761f1d304bcc79b6c5e60172f080c54ec
wudaidai <1665334372@qq.com>
2022-06-30 19:17:04 +0800

    san

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,22 @@
 <template>
   <div>
-    <!-- <MyYi></MyYi>
-    <MyYi2></MyYi2>
-    <MyYi3></MyYi3>
-    <MyYi4></MyYi4>
-    <MyYi5></MyYi5>
-    <MyYi6></MyYi6>
-    <MyYi7></MyYi7>
-    <MyYi8></MyYi8>
-    <MyYi9></MyYi9>
-    <MyYi10></MyYi10>
-    <MyYi11></MyYi11>
-    <MyYi12></MyYi12>
-    <MyYi13></MyYi13>
-    <MyYi14></MyYi14>
-    <MyYi15></MyYi15> -->
-    <MyYi16></MyYi16>
+
+    <h1 v-show="isShow">我是h1</h1>
+    <h2 v-if="isOk">我是h2</h2>
+    
+     <p v-if="age >= 18">成年了</p>
+     <p v-else>未成年</p>
   </div>
 </template>
 
 <script>
-// import MyYi from './components/MyYi.vue'
-// import MyYi2 from './components/MyYi2.vue'
-// import MyYi3 from './components/MyYi3.vue'
-// import MyYi4 from './components/MyYi4.vue'
-// import MyYi5 from './components/MyYi5.vue'
-// import MyYi6 from './components/MyYi6.vue'
-// import MyYi7 from './components/MyYi7.vue'
-// import MyYi8 from './components/MyYi8.vue'
-// import MyYi9 from './components/MyYi9.vue'
-// import MyYi10 from './components/MyYi10.vue'
-// import MyYi11 from './components/MyYi11.vue'
-// import MyYi12 from './components/MyYi12.vue'
-// import MyYi13 from './components/MyYi13.vue'
-// import MyYi14 from './components/MyYi14.vue'
-// import MyYi15 from './components/MyYi15.vue'
-import MyYi16 from './components/MyYi16.vue'
-export default{
-components:{
-  // MyYi,
-  // MyYi2,
-  // MyYi3,
-  // MyYi4,
-  // MyYi5,
-  // MyYi6,
-  // MyYi7,
-  // MyYi8,
-  // MyYi9,
-  // MyYi10,
-  // MyYi11,
-  // MyYi12,
-  // MyYi13,
-  // MyYi14,
-  // MyYi15,
-  MyYi16,
-},
+export default {
+  data() {
+    return {
+        isShow: false,
+        isOk: false,
+        age: 2
+    }
+  }
 }
 </script>
-
-<style scoped>
-</style>
--- src/components/MyYi.vue
+++ src/components/MyYi.vue
@@ -1,26 +1,21 @@
 <template>
-  <div>{{msg}}
-  <h1>{{obj.name}}</h1>
-  <h1>{{obj.age}}</h1>
-  <h1>{{obj.age>=18?'上网去':'回家'}}</h1>
-  </div>
+<div>
+  <p v-text='str'></p>
+  <p v-html='str'></p>
+  <p v-html='str'>{{str}}</p>
+</div>
 </template>
 
 <script>
 export default {
-name:'App',
-data(){
-    return{
-        msg:'你好你好',
-        obj: {
-            name:'小弟',
-            age:16
-        }
-    }
-}
+ data () {
+ return {
+  str:'<span>我是一个span标签</span>',
+ }
+ },
+ methods: {},
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
