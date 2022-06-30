efe63667701c8aa1e6c24740c0ed21acd2473531
wudaidai <1665334372@qq.com>
2022-06-30 19:34:38 +0800

    品牌案例完成

---
+++ src/components/App3.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="app">
+      <p>学生姓名</p>
+      <ul>
+        <li v-for="(item, index) in arr" :key="item">
+          {{ index }}  {{ item }}
+        </li>
+      </ul>
+      <p>学生详细信息</p>
+      <ul>
+        <li v-for="obj in stuArr" :key="obj.id">
+          <span>{{ obj.name }}</span>
+          <span>{{ obj.sex }}</span>
+          <span>{{ obj.hobby }}</span>
+        </li>
+      </ul>
+
+      <p>老师信息</p>
+      <div v-for="(value, key) in tObj" :key="value">
+        {{ key }}  {{ value }}
+      </div>
+
+      <p>序号</p>
+      <div v-for="i in count" :key="i">{{ i }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ["小明", "小欢欢", "大黄"],
+      stuArr: [
+        {
+          id: 1001,
+          name: "孙悟空",
+          sex: "男",
+          hobby: "吃桃子",
+        },
+        {
+          id: 1002,
+          name: "猪八戒",
+          sex: "男",
+          hobby: "背媳妇",
+        },
+      ],
+      tObj: {
+        name: "小黑",
+        age: 18,
+        class: "1期",
+      },
+      count: 10,
+    };
+  },
+};
+</script>
---
+++ src/components/App5.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 动态绑定style 语法 v-bind:style="对象" 
+  			v-bind:style = '{css属性名: 值}'-->
+    <p v-bind:style="{ color: 'red', fontSize: '20px' }">小弟</p>
+    <p v-bind:style="{ color: colorValue, fontSize: `${fontSize}px` }">
+      小弟
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colorValue: "green",
+      fontSize: 60,
+    };
+  },
+};
+</script>
+
+<style></style>
