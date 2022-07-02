b00b6938598aaf6dae62f90147b61e25e3076385
wudaidai <1665334372@qq.com>
2022-07-02 17:55:28 +0800

    wancheng

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <table class="tb">
       <tr>
-        <th><input type="checkbox" v-model="all"/>全选</th>
+        <th><input type="checkbox" v-model="all" />全选</th>
         <th>商品</th>
         <th>单价</th>
         <th>数量</th>
@@ -10,13 +10,18 @@
         <th>操作</th>
       </tr>
       <!-- 循环渲染的元素tr -->
-      <tr v-for="item in list" :key='item.id'>
+      <tr v-for="item in list" :key="item.id">
         <td><input type="checkbox" v-model="item.c" /></td>
-        <td>{{item.name}}</td>
-        <td>{{item.jia}}</td>
-        <td><span @click='(item.num<=1 ? 1:item.num--)'>-</span><input type="text" v-model="item.num" /><span @click='item.num++'>+</span></td>
-        <td>{{item.jia*item.num}}</td>
-        <td><button @click='del(item.id)'>删除</button></td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.jia }}</td>
+        <td>
+          <span @click="item.num <= 1 ? 1 : item.num--">-</span
+          ><input type="text" v-model="item.num" /><span @click="item.num++"
+            >+</span
+          >
+        </td>
+        <td>{{ item.jia * item.num }}</td>
+        <td><button @click="del(item.id)">删除</button></td>
       </tr>
 
       <tr v-if="list.length === 0">
@@ -24,12 +29,12 @@
       </tr>
     </table>
     <br />
-    <button @click='shan'>删除选中商品</button>
-    <button>清理购物车</button>
+    <button @click="shan">删除选中商品</button>
+    <button @click="qing">清理购物车</button>
     <br />
     <div style="margin-top: 20px">
       <h2>统计</h2>
-      <p>已经选中商品件数{{jianshu}}</p>
+      <p>已经选中商品件数{{ jianshu }}</p>
       <p>总价{{ zong }}</p>
     </div>
   </div>
@@ -40,11 +45,12 @@ export default {
   data() {
     return {
       list: [
-        { id: 1, name: "奔驰",c:false, jia:100,num:1, },
-        { id: 2, name: "宝马",c:false, jia:80,num:1, },
-        { id: 3, name: "奥迪",c:false, jia:60,num:1, },
-      ],c:false,
-      xuan:[]
+        { id: 1, name: "奔驰", c: false, jia: 100, num: 1 },
+        { id: 2, name: "宝马", c: false, jia: 80, num: 1 },
+        { id: 3, name: "奥迪", c: false, jia: 60, num: 1 },
+      ],
+      c: false,
+      xuan: [],
     };
   },
   methods: {
@@ -52,36 +58,42 @@ export default {
       // 删除按钮 - 得到索引, 删除数组里元素
       this.list.splice(index, 1);
     },
-    shan(){
-      this.list = this.list.filter((item) =>{
-        return item.c === false
-      })
-    }
-  },
-    computed: {
-      all:{
-         set(val){
-          this.list.forEach((ele)=>(ele.c=val))
-         },
-         get(){
-          return this.list.every((ele)=>ele.c)
-         }
-      },
-    zong() {
-      return this.list.filter((item) =>{
-        return item.c === true
-      }).reduce((sum,item) => {
-        return sum+=(item.jia*item.num)
-      },0)
+    shan() {
+      this.list = this.list.filter((item) => {
+        return item.c === false;
+      });
     },
-    jianshu(){
-      return this.list.filter((item) =>{
-        return item.c ===true
-      }).reduce((sum,item) => {
-        return sum+=item.num
-      },0)
-    }
-  
+    qing() {
+      this.list =[];
+    },
+  },
+  computed: {
+    all: {
+      set(val) {
+        this.list.forEach((ele) => (ele.c = val));
+      },
+      get() {
+        return this.list.every((ele) => ele.c === true);
+      },
+    },
+    zong() {
+      return this.list
+        .filter((item) => {
+          return item.c === true;
+        })
+        .reduce((sum, item) => {
+          return (sum += item.jia * item.num);
+        }, 0);
+    },
+    jianshu() {
+      return this.list
+        .filter((item) => {
+          return item.c === true;
+        })
+        .reduce((sum, item) => {
+          return (sum += item.num);
+        }, 0);
+    },
   },
 };
 </script>
