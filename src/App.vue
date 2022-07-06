2bd8ef9216ebf93bc86dae6d22ca3c92fc82d913
wudaidai <1665334372@qq.com>
2022-07-06 17:48:15 +0800

    新建

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
-  <div>
-    <ReFs :son='Son'></ReFs>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import ReFs from "./components/ReFs.vue";
-export default {
-  data () {
-    return {
-      Son:'艾欧尼亚，昂扬不灭'
-    }
-  },
-  components: {
-     ReFs,
-  },
 
+export default {
 };
 </script>
